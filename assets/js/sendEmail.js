26d89ef8073ac72013dc914e2e0dca1361e2bb29
Claire Potter <clairepotter019@gmail.com>
2022-01-11 12:05:55 +0000

    Add sweet alert pop-up, change interests to LinkedIn and add updated Resume

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,9 +7,27 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            //SweetAlert used to format the alert
+            Swal.fire(
+            {
+             title: "Success!",
+             text: "Your message has been sent successfully.",
+             icon: "success",
+             confirmButtonText: "Close",
+             confirmButtonColor: "#3b3939"
+              });
         },
         function(error) {
             console.log("FAILED", error);
+            //SweetAlert used to format the alert
+            Swal.fire(
+            {
+             title: "Oh Dear!",
+             text: "An error has occured, your message has not been sent.",
+             icon: "error",
+             confirmButtonText: "Close",
+             confirmButtonColor: "#3b3939"
+              });
         }
     );
     return false;  // To block from loading a new page
